224914566f67a9c361528198564a284141757895
Ayman Aamam <57199947+Ayman-aa@users.noreply.github.com>
2024-11-22 15:32:25 +0000

    [fixed]: the nginx service was down because of a typo in opened port, permissions for the ssl certificates and a naming convension in the index file

--- srcs/requirements/nginx/tools/server.sh
+++ srcs/requirements/nginx/tools/server.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Check if environment variables are set
-if [ -z "$SSL_CERTIFICATE" ] || [ -z "$SSL_CERTIFICATE_KEY" ] || [ -z "$DOMAIN_NAME" ]; then
+if [ -z "$SSL_CERTIFICATE" ] || [ -z "$SSL_CERTIFICATE_KEY" ] || [ -z "$LOGIN" ]; then
     echo "Error: Required environment variables are not set"
-    echo "Please ensure SSL_CERTIFICATE, SSL_CERTIFICATE_KEY, and DOMAIN_NAME are set in .env file"
+    echo "Please ensure SSL_CERTIFICATE, SSL_CERTIFICATE_KEY, and LOGIN are set in .env file"
     exit 1
 fi
 
 # Generate a self-signed SSL certificate if it doesn't exist
-# ${DOMAIN_NAME} in normal case
 if [ ! -f "$SSL_CERTIFICATE" ] || [ ! -f "$SSL_CERTIFICATE_KEY" ]; then
     echo "Generating SSL certificate..."
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout "$SSL_CERTIFICATE_KEY" -out "$SSL_CERTIFICATE" \
-    -subj "/C=MA/L=BG/O=UM6P/OU=1337/CN=${DOMAIN_NAME}"
+    -subj "/C=MA/L=Benguerir/O=UM6P/OU=1337/CN=localhost"
 fi
-
+chmod 600 /etc/ssl/private/nginx.key
+chmod 644 /etc/ssl/certs/nginx.crt
 # Replace environment variables in nginx config
 echo "Configuring nginx..."
-envsubst '${SSL_CERTIFICATE} ${SSL_CERTIFICATE_KEY} ${DOMAIN_NAME}' \
+envsubst '${SSL_CERTIFICATE} ${SSL_CERTIFICATE_KEY} ${LOGIN}' \
 < /etc/nginx/sites-enabled/default \
 > /etc/nginx/sites-enabled/default.tmp
 mv /etc/nginx/sites-enabled/default.tmp /etc/nginx/sites-enabled/default
